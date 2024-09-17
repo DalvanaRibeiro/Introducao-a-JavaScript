bf50e578c2944c8a02aa3e04da0736f9f21e3721
Dalvana <lopesribeirodalvana20@gmail.com>
2024-09-17 12:47:01 -0300

    atualização na pasta array

---
+++ Arrays/acessandoIndiceArray.js
@@ -0,0 +1,14 @@
+/*
+###################################################################################################
+Título: Usando arrays para descobrir qual raça esta associada a um determinado índice
+---------------------------------------------------------------------------------------------------
+Este exercício ajuda a praticar a manipulação de arrays no acesso de valores atribuídos a um determin
+ado ídice.
+###################################################################################################
+*/
+
+// Cria um array com o nome racasDeCachorro e atribuindo (=) a ela algumas raças
+const racasDeCachorro = ["Labrador", "Bulldog", "Beagle", "Poodle", "Dachshund"];
+
+// Imprimindo no console a raça correspondente à posição escolhida
+console.log(racasDeCachorro[4]);
