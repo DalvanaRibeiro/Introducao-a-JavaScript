0441a052725a2134a33339e516ab048ee407a1ee
Dalvana <lopesribeirodalvana20@gmail.com>
2024-09-19 20:55:10 -0300

    atualizando o Repositório

--- Códigos 05 - Condicionais/estadodeContaBancaria.js
+++ Códigos 05 - Condicionais/estadodeContaBancaria.js
@@ -3,14 +3,14 @@ let saldo = -200;
 
 // O if verifica se o saldo é maior que 0 (saldo positivo)
 if (saldo > 0) {
-  console.log("Sua conta está positiva com saldo de: R$" + saldo);
+  console.log("Sua conta está positiva com saldo de: R$" + saldo)
 } 
 // Verifica se o saldo é zero
 else if (saldo === 0) {
-  console.log("Sua conta está zerada.");
+  console.log("Sua conta está zerada.")
 } 
 // Se o saldo for menor que 0, o cliente está com saldo negativo
 else {
-  console.log("Sua conta está negativa com saldo de: R$" + saldo);
-  console.log("Recomenda-se fazer um depósito para regularizar.");
+  console.log("Sua conta está negativa com saldo de: R$" + saldo)
+  console.log("Recomenda-se fazer um depósito para regularizar.")
 }
--- Códigos 05 - Condicionais/ex2.js
+++ Códigos 05 - Condicionais/ex2.js
@@ -1,4 +1,4 @@
-// Solicita ao usuário que insira dois números
+/*// Solicita ao usuário que insira dois números
 let numero1 = parseFloat(prompt("Digite o primeiro número:"));
 let numero2 = parseFloat(prompt("Digite o segundo número:"));
 
@@ -8,3 +8,35 @@ if (numero1 === numero2) {
 } else {
   alert("Os números são diferentes.");
 }
+*/
+
+// Criando uma função
+
+function comparaNumeros(numero1, numero2) {
+    if (numero1 === numero2) {
+        return "Os números são iguais rpzd moleque piranha."
+} else{
+    return "Os números são diferentes rpzd."
+}
+
+}
+
+// Definir as variáveis de entrada (numero1 e numero2)
+let numero1 = 20
+let numero2 = 20
+
+// Chamada da Função
+resultado = comparaNumeros(numero1, numero2)
+console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
